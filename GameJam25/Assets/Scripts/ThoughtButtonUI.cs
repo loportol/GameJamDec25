4d2ecef28e42ed2b571903f60fc45786c8d8cc13
d3adgoose <kayleem0201@gmail.com>
2025-12-28 20:19:29 -0800

    Added a ThoughtButtonUI script

--- GameJam25/Assets/Scripts/ThoughtButtonUI.cs
+++ GameJam25/Assets/Scripts/ThoughtButtonUI.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class NewEmptyCSharpScript
+public class ThoughtButtonUI : MonoBehaviour
 {
-    
+    public TextMeshProUGUI buttonText;
+
+    private bool isCorrect;
+    private System.Action<bool> onSelected;
+
+    public void Setup(string text, bool correct, System.Action<bool> callback)
+    {
+        buttonText.text = text;
+        isCorrect = correct;
+        onSelected = callback;
+    }
+
+    public void OnClick()
+    {
+        onSelected?.Invoke(isCorrect);
+        Destroy(gameObject); // Remove button after selection
+    }
 }
+
